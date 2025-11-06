770fada7011519aa9211e4963536496c979113c0
jpfain <jpfain@gmail.com>
2025-11-06 14:57:21 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,12 +1,5 @@
-const CACHE_NAME = "jehovahs-view-of-time-v1";
-const FILES_TO_CACHE = ["./", "./index.html", "./manifest.json", "./icons/icon-192.png", "./icons/icon-512.png"];
-self.addEventListener("install", (event) => {
-  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)).then(() => self.skipWaiting()));
-});
-self.addEventListener("activate", (event) => {
-  event.waitUntil(caches.keys().then((keyList) => Promise.all(keyList.map((key) => { if (key !== CACHE_NAME) return caches.delete(key);} ))));
-  self.clients.claim();
-});
-self.addEventListener("fetch", (event) => {
-  event.respondWith(caches.match(event.request).then((response) => response || fetch(event.request)));
-});
+const CACHE_NAME="jehovahs-view-of-time-v1";
+const FILES_TO_CACHE=["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
+self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)).then(()=>self.skipWaiting()));});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>{if(k!==CACHE_NAME)return caches.delete(k);})))) ;self.clients.claim();});
+self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
