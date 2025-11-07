21b56cbd3623302b0fff795a1bc6d718f48bd352
jpfain <jpfain@gmail.com>
2025-11-07 11:43:20 -0500

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,6 @@
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open('jvt-v1').then(cache => {
-      return cache.addAll(['./', './index.html', './manifest.json']);
-    })
-  );
+  e.waitUntil(caches.open('v1').then(cache => cache.addAll(['./index.html'])));
 });
 self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => {
-      return resp || fetch(e.request);
-    })
-  );
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
 });
